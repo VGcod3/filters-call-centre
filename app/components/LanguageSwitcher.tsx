ec4bd8b5ebd51383092f73dae64e3f9c1e1094ff
Artem-Front-End <demchkoa@gmaol.com>
2024-04-27 16:11:24 +0300

    Language Switcher updates

--- app/components/LanguageSwitcher.tsx
+++ app/components/LanguageSwitcher.tsx
@@ -3,7 +3,7 @@ import * as SwitchPrimitives from "@radix-ui/react-switch";
 
 import { cn } from "~/lib/utils";
 import { useTranslation } from "react-i18next";
-import { useNavigate } from "@remix-run/react";
+import { Link, useSearchParams, useNavigation } from "@remix-run/react";
 import { useDirection } from "~/utils/useDirection";
 
 const LanguageSwitcher = React.forwardRef<
@@ -11,40 +11,54 @@ const LanguageSwitcher = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
   // eslint-disable-next-line react/prop-types
 >(({ className, ...props }, ref) => {
-  const { i18n, t } = useTranslation();
-  const navigate = useNavigate();
+  const { t } = useTranslation();
   const isRTL = useDirection();
+  const navigation = useNavigation();
+  const [searchParams] = useSearchParams();
+
+  const isSubmitting =
+    navigation.state !== "idle" &&
+    navigation.location.search.includes("lng=" + (isRTL ? "en" : "he"));
+
+  searchParams.set("lng", isRTL ? "en" : "he");
+  const to = "?" + searchParams.toString();
 
   return (
     <SwitchPrimitives.Root
+      {...props}
       className={cn(
         "peer relative inline-flex h-[24px] w-[56px] shrink-0 bg-gray-100 border border-gray-300 cursor-pointer items-center rounded-full shadow-sm transition-colors",
         "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background",
+        isSubmitting && "cursor-not-allowed opacity-50",
         className
       )}
-      {...props}
-      checked={isRTL}
-      onCheckedChange={() => {
-        const newLanguage = i18n.language === "en" ? "he" : "en";
-        navigate(`?lng=${newLanguage}`);
-      }}
+      disabled={isSubmitting}
       ref={ref}
-      dir="ltr"
+      asChild
     >
-      <SwitchPrimitives.Thumb asChild>
-        <div
+      <Link to={to}>
+        <SwitchPrimitives.Thumb asChild>
+          <div
+            className={cn(
+              "pointer-events-none flex items-center justify-center text-xs h-5 w-[26px] rounded-full bg-blue-600 text-white shadow-lg ring-0 transition-transform",
+              isRTL ? "translate-x-[-2px]" : "translate-x-0.5"
+            )}
+          >
+            {isRTL ? t("sidebar.language.he") : t("sidebar.language.en")}
+          </div>
+        </SwitchPrimitives.Thumb>
+        <span
           className={cn(
-            "pointer-events-none flex items-center justify-center text-xs h-5 w-[26px] rounded-full bg-blue-600 text-white shadow-lg ring-0 transition-transform",
-            isRTL ? "translate-x-[27px]" : "translate-x-0.5"
+            "text-xs absolute text-gray-800",
+            isRTL ? "left-1.5" : "right-1.5"
           )}
         >
-          {isRTL ? t("sidebar.language.he") : t("sidebar.language.en")}
-        </div>
-      </SwitchPrimitives.Thumb>
-      <span className={cn("text-xs absolute text-gray-800", isRTL ? "left-[6px]" : "right-[6px]")} >{isRTL ? "EN" : "HE"}</span>
+          {isRTL ? "EN" : "HE"}
+        </span>
+      </Link>
     </SwitchPrimitives.Root>
   );
 });
-LanguageSwitcher.displayName = SwitchPrimitives.Root.displayName;
+LanguageSwitcher.displayName = "LanguageSwitcher";
 
 export { LanguageSwitcher };
