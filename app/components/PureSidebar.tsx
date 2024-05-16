fb8d9b59370ad683cf5414b4e663b80a5d5f4ea6
Sam Apter <sam@sapter.com>
2024-05-16 13:49:25 +0200

    clean up sidebar-style state

--- app/components/PureSidebar.tsx
+++ app/components/PureSidebar.tsx
@@ -8,16 +8,16 @@ import { Avatar, AvatarFallback } from "./ui/avatar";
 import { useReducer } from "react";
 
 enum Display {
-  Full = 'full',
-  Floating = 'floating',
-  Hidden = 'hidden',
+  Full = "full",
+  Floating = "floating",
+  Hidden = "hidden",
 }
 
 interface SidebarState {
   display: Display;
   isHovered: boolean;
   isTransition: boolean;
-  isFloating: boolean;
+  sidebarStyle: "full" | "floating";
 }
 
 type SidebarAction =
@@ -26,44 +26,59 @@ type SidebarAction =
   | { type: "opened_full_mode" }
   | { type: "hovered_sidebar" }
   | { type: "stoped_transition" }
-  | { type: "closed_sidebar" }
+  | { type: "closed_sidebar" };
 
-  const saveDisplayToCookie = (display:Display) => {
-    const cookieValue = encodeURIComponent(display);
-    document.cookie = `display=${cookieValue}; path=/`;
-  };
+const saveDisplayToCookie = (display: Display) => {
+  const cookieValue = encodeURIComponent(display);
+  document.cookie = `display=${cookieValue}; path=/`;
+};
 
-  const getDisplayFromCookie = () => {
-    if(typeof document !== 'undefined'){
-      const cookies = document.cookie.split(';');
-      const displayCookie = cookies.find(cookie => cookie.trim().startsWith('display='));
-      if (displayCookie) {
-        const displayValue = decodeURIComponent(displayCookie.split('=')[1]);
-        return displayValue as Display;
-      }
+const getDisplayFromCookie = () => {
+  if (typeof document !== "undefined") {
+    const cookies = document.cookie.split(";");
+    const displayCookie = cookies.find((cookie) =>
+      cookie.trim().startsWith("display=")
+    );
+    if (displayCookie) {
+      const displayValue = decodeURIComponent(displayCookie.split("=")[1]);
+      return displayValue as Display;
     }
-    return Display.Hidden;
-  };
+  }
+  return Display.Hidden;
+};
 
-export const sidebarReducer = (state: SidebarState, action: SidebarAction) => {
+export const sidebarReducer = (
+  state: SidebarState,
+  action: SidebarAction
+): SidebarState => {
   switch (action.type) {
     case "hovered_open_button_or_edge":
-      return { ...state, display: Display.Floating, isTransition: true, isFloating: true};
+      return {
+        ...state,
+        display: Display.Floating,
+        isTransition: true,
+        sidebarStyle: "floating",
+      };
     case "unhovered":
       if (state.display === Display.Full) return { ...state, isHovered: false };
       return { ...state, display: Display.Hidden };
     case "hovered_sidebar":
       if (state.display === Display.Full) return { ...state, isHovered: true };
-      return state
+      return state;
     case "opened_full_mode":
       saveDisplayToCookie(Display.Full);
-      return { ...state, display: Display.Full, isHovered: true, isFloating: false }
+      return {
+        ...state,
+        display: Display.Full,
+        isHovered: true,
+        sidebarStyle: "full",
+      };
     case "closed_sidebar":
       saveDisplayToCookie(Display.Hidden);
-      return { ...state, display: Display.Hidden, isHovered: false }
+      return { ...state, display: Display.Hidden, isHovered: false };
     case "stoped_transition":
       if (state.display === Display.Full) return state;
-      return { ...state, isTransition: false}
+      return { ...state, isTransition: false };
     default:
       throw new Error("Invalid action type");
   }
@@ -77,16 +92,21 @@ export const PureSidebar = () => {
     display: getDisplayFromCookie(),
     isHovered: false,
     isTransition: true,
-    isFloating: false,
+    sidebarStyle: "full",
   });
 
   return (
     <div>
       <div
-        className={cn("absolute top-1 pl-5 pr-7 pt-2 pb-10", isRTL ? 'right-0' : 'left-0')}
+        className={cn(
+          "absolute top-1 pl-5 pr-7 pt-2 pb-10",
+          isRTL ? "right-0" : "left-0"
+        )}
         onMouseEnter={() => dispatch({ type: "hovered_open_button_or_edge" })}
         onMouseLeave={({ clientX, clientY }) => {
-          const isInvalidPosition = isRTL ? clientX > window.innerWidth - 84 && clientY > 0 : clientX < 84 && clientY > 0;
+          const isInvalidPosition = isRTL
+            ? clientX > window.innerWidth - 84 && clientY > 0
+            : clientX < 84 && clientY > 0;
           if (isInvalidPosition) return;
           dispatch({ type: "unhovered" });
         }}
@@ -103,34 +123,47 @@ export const PureSidebar = () => {
         )}
       </div>
 
-      {
-        state.display === Display.Floating || state.display === Display.Full
-          ? null
-          : <div
-            className={cn("fixed bg-transparent top-[84px] w-2.5 h-full", isRTL ? 'right-0' : 'left-0')}
-            onMouseEnter={() => dispatch({ type: "hovered_open_button_or_edge" })}
-          />
-      }
+      {state.display === Display.Floating ||
+      state.display === Display.Full ? null : (
+        <div
+          className={cn(
+            "fixed bg-transparent top-[84px] w-2.5 h-full",
+            isRTL ? "right-0" : "left-0"
+          )}
+          onMouseEnter={() => dispatch({ type: "hovered_open_button_or_edge" })}
+        />
+      )}
       <nav
         className={cn(
           "w-[288px] border border-gray-300 border-r-2 pl-6 pr-6 bg-white",
-          state.isTransition ? "transition-all duration-500 ease-in-out" : "opacity-0",
-          state.display === Display.Full && "translate-x-0 h-screen",
-          state.display === Display.Floating && "absolute top-[8%] bottom-[1%] rounded-xl",
-          state.display === Display.Floating && (isRTL ? "-translate-x-3" : "translate-x-3"),
-          state.display === Display.Hidden ? (isRTL ? "translate-x-[100%]" : "translate-x-[-100%]") : "",
-          state.display === Display.Hidden ? (state.isFloating ? 'absolute top-[8%] bottom-[1%] rounded-xl' : 'h-screen') : '',
+          state.isTransition
+            ? "transition-all duration-500 ease-in-out"
+            : "opacity-0",
+          state.display === Display.Full && "translate-x-0",
+          state.display === Display.Floating &&
+            (isRTL ? "-translate-x-3" : "translate-x-3"),
+          state.display === Display.Hidden
+            ? isRTL
+              ? "translate-x-[100%]"
+              : "translate-x-[-100%]"
+            : "",
+          state.sidebarStyle === "floating"
+            ? "absolute top-[8%] bottom-[1%] rounded-xl"
+            : "h-screen",
           isRTL ? "right-0" : "left-0"
         )}
         onMouseEnter={({ clientX }) => {
-          const shouldReturn = isRTL ? clientX >= window.innerWidth - 84 : clientX <= 84;
+          const shouldReturn = isRTL
+            ? clientX >= window.innerWidth - 84
+            : clientX <= 84;
           if (shouldReturn) return;
           dispatch({ type: "hovered_sidebar" });
         }}
         onMouseLeave={({ clientX, clientY }) => {
           const shouldReturn = isRTL
-            ? (clientX > window.innerWidth - 15) ||  (clientX > window.innerWidth - 84 && clientY <= 85)
-            : (clientX < 15) || (clientX < 84 && clientY <= 85);
+            ? clientX > window.innerWidth - 15 ||
+              (clientX > window.innerWidth - 84 && clientY <= 85)
+            : clientX < 15 || (clientX < 84 && clientY <= 85);
 
           if (shouldReturn) return;
           dispatch({ type: "unhovered" });
@@ -139,15 +172,15 @@ export const PureSidebar = () => {
         <div className="flex justify-between items-center mt-10">
           <div className="flex gap-4">
             <Avatar className="w-12 h-12">
-              <AvatarFallback className="bg-gray-800 text-white font-semibold">SW</AvatarFallback>
+              <AvatarFallback className="bg-gray-800 text-white font-semibold">
+                SW
+              </AvatarFallback>
             </Avatar>
             <div className="whitespace-nowrap overflow-hidden">
               <h2 className="text-gray-900 text-[16px] font-semibold overflow-hidden overflow-ellipsis">
                 Sarah Wilson
               </h2>
-              <h1 className="text-gray-900 opacity-80 text-sm">
-                Admin
-              </h1>
+              <h1 className="text-gray-900 opacity-80 text-sm">Admin</h1>
             </div>
           </div>
           {state.display === Display.Full && state.isHovered && (
@@ -162,7 +195,11 @@ export const PureSidebar = () => {
           )}
         </div>
         <div className="flex items-center justify-center w-full">
-          <Button asChild className="bg-red-500" onClick={() => dispatch({type: "stoped_transition"})}>
+          <Button
+            asChild
+            className="bg-red-500"
+            onClick={() => dispatch({ type: "stoped_transition" })}
+          >
             <Link to={`?lng=${i18n.language === "en" ? "he" : "en"}`}>
               Switch
             </Link>
@@ -171,4 +208,4 @@ export const PureSidebar = () => {
       </nav>
     </div>
   );
-}; 
+};
